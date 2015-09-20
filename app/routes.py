e478797d98f77da97af1d5ffe0aa15a1a6e51626
Ryan Zhou <ryan.xingyu.zhou@gmail.com>
2015-09-19 23:58:21 -0400

    added api file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from app import app
-from flask import render_template,flash,redirect
+from app import app, db, lm, oid
+from flask import render_template,flash,redirect,session,url_for,request,g
+from flask.ext.login import login_user,logout_user,current_user,login_required
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
@@ -22,7 +24,10 @@ def index():
                             posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login Requested for OpenID="%s", remember_me=%s'
@@ -32,3 +37,7 @@ def login():
                             title='Sign In',
                             form=form,
                             providers=app.config['OPENID_PROVIDERS'])
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
